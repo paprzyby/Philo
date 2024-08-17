fce20951af87086cec7765b2b8e4ca4bfabff2e1
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-08-17 09:19:27 +0200

    created a struct for all the data

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 16:02:54 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/08/16 14:31:15 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/08/17 09:18:42 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,34 @@
 # include <stdbool.h>
 # include <limits.h>
 
-typedef struct	s_data
+typedef struct	s_fork
 {
-	int	philo_count;
-	int	forks_count;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_of_times;
-}				t_data;
+	pthread_mutex_t	fork;
+	int				id;
+}				t_fork;
 
 typedef struct	s_philo
 {
-	int	id;
-}				t_philo;
-
-typedef struct	s_fork
-{
 	pthread_t	philo;
 	int			id;
-}				t_fork;
+	long		meals;
+	bool		full;
+	long		last_meal;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+}				t_philo;
+
+typedef struct	s_data
+{
+	int		philo_count;
+	int		forks_count;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		num_of_times;
+	t_philo	*philos;
+	t_fork	*forks;
+}				t_data;
 
 int		error(char *message, t_data *list);
 bool	is_digit(char *str);
