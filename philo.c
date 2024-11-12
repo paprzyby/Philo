42a071515d52a57df605ab1b2adfd52c22e8c5e4
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2024-11-12 18:06:00 +0100

    started implementing the function ft_sleep

--- philo.c
+++ philo.c
@@ -6,13 +6,27 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 15:26:56 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/11/11 17:15:43 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/11/12 18:04:45 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int global_timestamp = 1;
+int	ft_sleep(long ms)
+{
+	struct timeval	time;
+	long			start;
+
+	if (gettimeofday(&time, NULL) != 0)
+		return (ft_putstr_fd("gettimeofday error\n", 2), 1);
+	start = time.tv_sec * 1000 + time.tv_usec / 1000;
+	while (gettimeofday(&time, NULL) != 0 && (time.tv_sec * 1000 + time.tv_usec
+			/ 1000 - start) < ms)
+		usleep(500);
+	if (gettimeofday(&time, NULL) != 0)
+		return (ft_putstr_fd("gettimeofday error\n", 2), 1);
+	return (0);
+}
 
 void	*checker_function(void *arg)
 {
@@ -22,17 +36,30 @@ void	*checker_function(void *arg)
 	return (0);
 }
 
+int	check_dead_flag(t_philo *philos)
+{
+	pthread_mutex_lock(philos->dead_lock);
+	if (philos->dead_flag == true)
+		return (pthread_mutex_unlock(philos->dead_lock), 1);
+	pthread_mutex_unlock(philos->dead_lock);
+	return (0);
+}
+
 void	*philo_function(void *arg)
 {
 	t_philo *philos = (t_philo *)arg;
 
-	while (global_timestamp != philos->id)
-		usleep(200000);
-	pthread_mutex_lock(philos->write_lock);
-	printf("(timestamp_in_ms) %d is thinking\n", philos->id);
-	global_timestamp++;
-	pthread_mutex_unlock(philos->write_lock);
-	return (0);
+	if (philos->id % 2 == 0)
+		ft_sleep(1);
+	while (1)
+	{
+		pthread_mutex_lock(philos->write_lock);
+		printf("(timestamp_in_ms) %d is thinking\n", philos->id);
+		pthread_mutex_unlock(philos->write_lock);
+		if (check_dead_flag(philos))
+			return (arg);
+	}
+	return (arg);
 }
 
 int	philo(t_data *data)
